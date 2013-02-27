3b94edc237573ed47d67f7628fe10aeeab3ed731
Kasabian <pandascorp@mail.ru>
2013-02-27 15:05:56 +0200

    script

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -10,6 +10,7 @@
 		
 setInterval(function()
 {
+ $(".group1").colorbox({rel:'group1', current: "фото {current} из {total}"});
  var h = ($(window).height() - 500)/2;
  $("#main").css("margin-top",h) 
  $("#content_1").mCustomScrollbar("update")
